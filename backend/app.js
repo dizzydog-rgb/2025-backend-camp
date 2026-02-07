032ad84089520c0945b281d15dee9caa58ff6017
dizzydog-rgb <fox29582@yahoo.com.tw>
2026-02-07 16:29:55 +0800

    更新: 初階任務

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/healthcheck", (req, res) => {
   res.status(200).json({
     status: "success",
-    message: "OKKK",
+    message: "OK",
   });
 });
 app.use("/api/credit-package", creditPackageRouter);
